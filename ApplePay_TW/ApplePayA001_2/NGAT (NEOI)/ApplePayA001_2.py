528df712175e2d9d089a5480a3961293baef0e44
KateH726 <forpublich@gmail.com>
2022-06-01 09:29:29 +0800

    Update VISA applepay test card

--- ApplePay_TW/ApplePayA001_2/NGAT (NEOI)/ApplePayA001_2.py
+++ ApplePay_TW/ApplePayA001_2/NGAT (NEOI)/ApplePayA001_2.py
@@ -56,7 +56,7 @@ if (Result):
 				TagDFEE26 = DL.GetTLV(alldata,"DFEE26")
 				
 			# Tag 57					
-				Result = DL.Check_StringAB(Tag57, '57134817499130371965D23122260000016799993F')
+				Result = DL.Check_StringAB(Tag57, '57134831961250122892D23122010000099699993F')
 				if Result == True:
 					DL.SetWindowText("blue", "Tag 57: PASS")
 				else:
@@ -98,7 +98,7 @@ if (Result):
 				TagDFEE26 = DL.GetTLV(alldata,"DFEE26")
 				
 			# Tag 57					
-				Result = DL.Check_StringAB(Tag57, '57134817499130371965D23122260000016799993F')
+				Result = DL.Check_StringAB(Tag57, '57134831961250122892D23122010000099699993F')
 				if Result == True:
 					DL.SetWindowText("blue", "Tag 57: PASS")
 				else:

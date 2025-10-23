0ff9f8c517b270a1fef1f8a135888122ace980be
KateH726 <forpublich@gmail.com>
2025-10-23 16:32:34 +0800

    Update ECP 2.0 test case to add SWARTHMORE card
    Update ECP 2.0 test case to add SWARTHMORE card

---
+++ Blackboard transact credential_2/BTS014-T/NGAT/NEOII/BTS014-T.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import time
+RetOfStep = True
+Result= True
+
+Key='0123456789abcdeffedcba9876543210'
+MacKey='0123456789abcdeffedcba9876543210'
+PAN=''
+strKey = '0123456789ABCDEFFEDCBA9876543210'
+
+# Get Data Encryption (C7-37)
+if (Result):
+	RetOfStep = DL.SendCommand('Get Data Encryption (C7-37)')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("C7 00 00 01 00")
+		if Result == False:
+			DL.SetWindowText("red", "Data encryption should be disabled...C7-37 = 00")
+		
+# Get account DUKPT encryption type (C7-33)
+if (Result):
+	RetOfStep = DL.SendCommand('Get account DUKPT encryption type (C7-33)')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("C7 00 00 01 00")
+		if Result == False:
+			DL.SetWindowText("red", "Data key should be TDES type...C7-33 = 00")
+
+# # (C7-6A) Get BlackBoard Private Key Hash
+# if (Result):
+	# RetOfStep = DL.SendCommand('(C7-6A) Get BlackBoard Private Key Hash')
+	# if (RetOfStep):
+		# Result = Result and DL.Check_RXResponse("C7 00 00 20")
+		# if Result == False:
+			# DL.SetWindowText("red", "Reader should load BlackBoard Private Key first...")
+
+# # (C7-6A) Get BlackBoard LTPK Hash
+# if (Result):
+	# RetOfStep = DL.SendCommand('(C7-6A) Get BlackBoard LTPK Hash')
+	# if (RetOfStep):
+		# Result = Result and DL.Check_RXResponse("C7 00 00 20")	
+		# if Result == False:
+			# DL.SetWindowText("red", "Reader should load BlackBoard LTPK first...")	
+			
+			
+# (C7-6D) Load Campus Card Key Block / Retrieve Key Block Info
+if (Result):
+	RetOfStep = DL.SendCommand('(C7-6D) Load Campus Card Key Block / Retrieve Key Block Info')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("C7 00 00 3D")
+			
+			
+# (04-00) DFED3F Enable VAS Encryption
+if (Result):
+	RetOfStep = DL.SendCommand('(04-00) DFED3F Enable VAS Encryption')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("04 00 00 00")	
+		
+# (04-00) DFEF4B Enable Transact Output
+if (Result):
+	RetOfStep = DL.SendCommand('(04-00) DFEF4B Enable Transact Output')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("04 00 00 00")	
+
+# Poll on demand
+if (Result):
+	RetOfStep = DL.SendCommand('Poll on demand')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("01 00 00 00")	
+
+# Burst mode off	
+if (Result):
+	RetOfStep = DL.SendCommand('Burst mode off')
+	if (RetOfStep):
+		Result = Result and DL.Check_RXResponse("04 00 00 00")
+	
+if (Result):
+	for j in range (1, 7):
+		if j == 1:
+			RetOfStep = DL.SendCommand('(02-40) Apple VAS')
+		if j == 2:
+			RetOfStep = DL.SendCommand('(02 40) SmartTap 2')
+		if j == 3:
+			RetOfStep = DL.SendCommand('02-40 w/ AppleVAS -- VAS or PAY')
+		if j == 4:
+			RetOfStep = DL.SendCommand('02-40 w/ AppleVAS -- VAS and PAY')
+		if j == 5:
+			RetOfStep = DL.SendCommand('02-40 w/ SmartTAP -- VAS or PAY')
+		if j == 6:
+			RetOfStep = DL.SendCommand('02-40 w/ SmartTAP -- VAS and PAY')
+			
+		if (RetOfStep):
+			Result = Result and DL.Check_RXResponse("02 00 ** 21")
+			if (Result):
+				alldata = DL.Get_RXResponse(0)
+				ksn = DL.GetTLV(alldata,"DFEE12")	
+				
+				TagDFED44 = DL.GetTLV(alldata,"DFED44")
+				
+				TagDFEF4C = DL.GetTLV(alldata,"DFEF4C")
+				encDFEF4D = DL.GetTLV(alldata,"DFEF4D", 0)
+				decDFEF4D = DL.DecryptDLL(0,1, strKey, ksn, encDFEF4D)	
+				
+				TagFFEE06 = DL.GetTLV(alldata,"FFEE06")
+				TagFFEE08 = DL.GetTLV(alldata,"FFEE08")
+				Tag9F39 = DL.GetTLV(alldata,"9F39")
+				TagFFEE01 = DL.GetTLV(alldata,"FFEE01")
+				TagDFEE26 = DL.GetTLV(alldata,"DFEE26")
+						
+				# Tag DFEF4C-4D	
+				# Result = DL.Check_StringAB(TagDFEF4C, '00 00 00 00 30 00')
+				# if Result == True:
+					# DL.SetWindowText("blue", "Tag DFEF4C: PASS")
+				# else:
+					# DL.SetWindowText("red", "Tag DFEF4C: FAIL")
+									
+				# Result = DL.Check_StringAB(decDFEF4D, '00040000000355000000000003455900000000001001000001AC60010DFF28580000000000000000FCF86DBC68704101')
+				# if Result == True:
+					# DL.SetWindowText("blue", "Tag DFEF4D: PASS")
+				# else:
+					# DL.SetWindowText("red", "Tag DFEF4D: FAIL)
+					
+			# Tags 9F39/ FFEE01/ DFEE26 /DFED44
+				if (DL.Check_StringAB(Tag9F39, '07')): 
+					DL.SetWindowText("blue", "Tag 9F39: PASS")
+				else:
+					DL.SetWindowText("Red", "Tag 9F39: FAIL")
+				
+				if (DL.Check_StringAB(TagFFEE01, "DFEE300100")): 
+					DL.SetWindowText("blue", "Tag FFEE01: PASS")
+				else:
+					DL.SetWindowText("Red", "Tag FFEE01: FAIL")
+				
+				if (DL.Check_StringAB(TagDFEE26, '2100')):
+					DL.SetWindowText("blue", "Tag DFEE26: PASS")
+				else:
+					DL.SetWindowText("Red", "Tag DFEE26: FAIL")
+					
+				if (DL.Check_StringAB(TagDFED44, '901DAB2A8F9604')):
+					DL.SetWindowText("blue", "Tag DFED44: PASS")
+				else:
+					DL.SetWindowText("Red", "Tag DFED44: FAIL")
+				
+				time.sleep(1)

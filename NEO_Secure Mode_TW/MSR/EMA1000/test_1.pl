2fb1819e07bd7ecdbcdc7f70f742afd87d7dda79
Mindy725 <mindy.yang@idtechproducts.com>
2026-05-27 14:46:56 +0800

    NEO4 add cmd 29-04 w/ new value 00.
    NEO4 add cmd 29-04 w/ new value 00.

---
+++ NEO_Secure Mode_TW/MSR/EMA1000/test_1.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+# Using Packages
+use Env;
+use Cwd;
+
+# Defining Global Variables
+$start_dir = &cwd();
+$verbose = 1;
+
+# Parsing Command Line
+for(;;)						# forever loop
+{
+    last if($#ARGV < 0);
+    $argv = shift(@ARGV);
+    if(($argv eq "-h") ||
+       ($argv eq "-help") ||
+       ($argv eq "-?"))
+    {
+        printf("This program does an example test on the DockLight\n");
+	printf("Options:\n");
+	printf(" -h[elp]/-?             print this help blurb\n");
+	printf(" -v N                   set verbosity to N\n");
+        exit 1;
+    }
+    if(($argv eq "-v"))        { $verbose = shift(@ARGV); next; }
+
+}						# end forever
+
+`start /REALTIME docklight_scripting -r -m EMA1000.pts`;
+
+# Main Code
+
+
+
+# Exit
+exit 0;
